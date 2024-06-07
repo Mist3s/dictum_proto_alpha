997064ab576fb8658529face06a6178d09dde7be
Иванов Андрей <123881893+Mist3s@users.noreply.github.com>
2024-06-07 15:36:03 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dictum_proto',
-    version='0.0.1',
+    version='0.0.2',
     packages=find_packages(include=["dictum_proto", "dictum_proto.*"]),
     install_requires=[
         'grpcio==1.50.0',
