dce4e50abfc96356d9a772abc0d64a8375644f9a
Mist3s <ivanov.andrevv@gmail.com>
2024-06-07 15:23:41 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dictum_proto',
+    version='0.0.1',
+    packages=find_packages(include=["dictum_proto", "dictum_proto.*"]),
+    install_requires=[
+        'grpcio==1.50.0',
+        'grpcio-tools==1.50.0'
+    ],
+)
